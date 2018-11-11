af20c53b503b6dcf99dd58b07c9e136c52fcbe82
Pawan Panphinit <panphinit@gmail.com>
2018-11-11 21:29:07 +0700

    แก้ไข checkout_cart.php ให้แก้ไข amout left ได้

--- checkout_cart.php
+++ checkout_cart.php
@@ -6,7 +6,23 @@ $username = $_SESSION['username'];
 $shopping_cart_name = $username."_shopping_cart"; 
 
 if(isset($_COOKIE[$shopping_cart_name])){
+
+    $cookie_data = stripslashes($_COOKIE[$shopping_cart_name]);
+    $cart_data = json_decode($cookie_data, true);
+
+    $item_id_list = array_column($cart_data, 'item_id');
+
+    foreach($item_id_list as $id){
+        $query_string = "SELECT amount FROM menu WHERE id = ".$id ;
+        $before_amount = mysqli_fetch_array(mysqli_query($conn, $query_string));
+        $left = $before_amount['amount']-1;
+        $update_sql = "UPDATE menu SET amount = ".$left." WHERE id = ".$id;
+        mysqli_query($conn, $update_sql);
+    }
+
     setcookie($shopping_cart_name, "", time() - 3600);
+
 }
+
 header("Location: index.php");
 ?>
