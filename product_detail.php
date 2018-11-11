db35b34c8f7b1edb3f82b29ceb252a476cfc977b
Pawan Panphinit <panphinit@gmail.com>
2018-11-11 22:15:04 +0700

    แก้ไข product_detail.php ให้ Title มีชื่อสินค้า และปรับปรุงส่วนรายระเอียด

--- product_detail.php
+++ product_detail.php
@@ -7,7 +7,8 @@ include("conn.php");
 $idq = $_REQUEST['idq'];
 
 $sql_query_product = 'SELECT * FROM menu WHERE id = '.$idq;
-$query_food = mysqli_query($conn, $sql_query_product);
+$details = mysqli_fetch_array(mysqli_query($conn, $sql_query_product));
+$pName = $details['name'];
 
 $username = $_SESSION['username'];
 $shopping_cart_name = $username."_shopping_cart";
@@ -31,7 +32,7 @@ echo $total;
 <!DOCTYPE html>
 
 <html>
-<title>N&N Café</title>
+<title><?php echo $pName; ?> - NJ Network Devices</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -51,7 +52,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
   <div class="w3-container w3-display-container w3-padding-16">
     <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-    <h3 class="w3-wide"><b>N&N Café</b></h3>
+    <h3 class="w3-wide" ><a href="index.php" style="text-decoration: none"><b>NJ Network Devices</b></a></h3>
   </div>
   <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
     <a href="#" class="w3-bar-item w3-button">Menu</a>
@@ -101,11 +102,12 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 
   <!-- Product grid -->
   <div class="w3-row-padding">
-    <?php $details = mysqli_fetch_array($query_food); ?>
-    <h4> <?php echo $details['name']; ?> </h4>
+    <h4> <?php echo $pName ?> </h4>
     <img src="<?php echo $details['img']; ?>" style="width:50%; heigh:50%">
     <br><br>
     <div class="container">
+    <h6><b>รายละเอียด</b></h6>
+    <p><?php echo $details['des']; ?></p>
     <table>
       <tr>
         <td><b>เหลือ   </b></td>
@@ -121,8 +123,6 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
       
     </table>
     </div>
-    <h6><b>รายละเอียด</b></h6>
-    <p><?php echo $details['des']; ?></p>
   </div>
   
   <br><br>
@@ -232,12 +232,20 @@ if(isset($_COOKIE[$shopping_cart_name])){
               <h2 class="w3-wide" >Items in cart</h2>
               <p style="align:center">Please checkout.</p>
               <table align="center">
+              <tr>
+    <th>Img</th>
+    <th>Name</th>
+    <th>Amount</th>
+    <th>Price</th>
+  </tr>
+  <tr>
 <?php
   foreach($cart_data as $keys => $values){ ?>
-              <tr>
-                <td><?php echo $values["item_name"]; ?></td>
-                <td> <?php echo $values["item_price"]; ?> Baht.</td>
-                <td>X<?php echo $values["item_quantity"]; ?></td>
+  <th><img src="<?php echo $values["item_img"];; ?>" style="width: 30px; height: 30px;"></th>
+  <td><a href="product_detail.php?idq=<?php echo $values["item_id"]; ?>"><?php echo $values["item_name"]; ?></a></td>
+  <td align="right"><?php echo $values["item_quantity"]; ?>x</td>
+  <td align="right"> <?php echo $values["item_price"]; ?></td>
+    
                 
                 <!-- <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td> -->
                 <!-- <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td> -->
@@ -246,7 +254,7 @@ if(isset($_COOKIE[$shopping_cart_name])){
 <?php
     } ?>
     </table>
-    <p>Total: <?php echo $total; ?> Baht.</p>
+    <p align="right">Total: <b><?php echo $total; ?></b> Baht.</p>
 <form method="POST" action="checkout_cart.php" enctype="multipart/form-data">
                 <input type="submit" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('thecart').style.display='none'" value="Checkout">
                 </form>
