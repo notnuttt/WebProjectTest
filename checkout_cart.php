79ddef4081efb251aaee6464bd936b4abc2efe71
Pawan Panphinit <panphinit@gmail.com>
2018-11-11 21:44:49 +0700

    แก้ไขลบได้ตามจำนวนใน cart ไม่ใช่แค่ 1 อีกต่อไปแล้ว~ >////<

--- checkout_cart.php
+++ checkout_cart.php
@@ -10,12 +10,11 @@ if(isset($_COOKIE[$shopping_cart_name])){
     $cookie_data = stripslashes($_COOKIE[$shopping_cart_name]);
     $cart_data = json_decode($cookie_data, true);
 
-    $item_id_list = array_column($cart_data, 'item_id');
-
-    foreach($item_id_list as $id){
+    foreach($cart_data as $keys => $values){
+        $id = $cart_data[$keys]["item_id"];
         $query_string = "SELECT amount FROM menu WHERE id = ".$id ;
         $before_amount = mysqli_fetch_array(mysqli_query($conn, $query_string));
-        $left = $before_amount['amount']-1;
+        $left = $before_amount['amount'] - $cart_data[$keys]['item_quantity'];
         $update_sql = "UPDATE menu SET amount = ".$left." WHERE id = ".$id;
         mysqli_query($conn, $update_sql);
     }
